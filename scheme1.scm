9ce9de4192ecaef305b33cfc5632dc52d147950f
Jon Allen Boone <ipmonger@gmail.org>
2013-05-23 20:22:32 -0400

    updated comments and removed unused functions from scheme1.scm

--- scheme1.scm
+++ scheme1.scm
@@ -5,44 +5,50 @@
 ;;;;    Purpose:    Implements an interpreter for the proposition calculus using scheme.
 ;;;;                Grammar details below:
 ;;;;
-;;;;				< boolexp > ? #t | #f [boolean constants]
-;;;;				< boolexp > ? variable [boolean variables]
-;;;;				< boolexp > ? (and boolexp ... boolexp)
-;;;;				< boolexp > ? (or boolexp ... boolexp)
-;;;;				< boolexp > ? (not boolexp)
-;;;;				< boolexp > ? (implies boolexp boolexp) [P => Q, where P is the first 
+;;;;		    < boolexp > ? #t | #f [boolean constants]
+;;;;		    < boolexp > ? variable [boolean variables]
+;;;;		    < boolexp > ? (and boolexp ... boolexp)
+;;;;		    < boolexp > ? (or boolexp ... boolexp)
+;;;;		    < boolexp > ? (not boolexp)
+;;;;		    < boolexp > ? (implies boolexp boolexp) [P => Q, where P is the first 
 ;;;;							   argument and Q is the second argument]
-;;;;				< boolexp > ? (equiv boolexp boolexp) [P <=> Q, where P is the first 
+;;;;		    < boolexp > ? (equiv boolexp boolexp) [P <=> Q, where P is the first 
 ;;;;							   argument and Q is the second argument] 
 ;;;;
 ;;;;    Authors:    Jon Boone
-;;;;				Joshua Datko
-;;;;				Paul DeMicco
-;;;;				Joseph Heenan
+;;;;		    Joshua Datko
+;;;;		    Paul DeMicco
+;;;;		    Joseph Heenan
 ;;;;
 ;;;;    Created:    05/21/2013
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; beval provided by Professor Johnson
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define (beval expr env)
   (cond
     ((boolean? expr) expr)
     ((symbol? expr) (lookup expr env))
-    ((conjuct? expr) (beval-and expr env))
-    ((disjuct? expr) (beval-or expr env))
+    ((conjunct? expr) (beval-and expr env))
+    ((disjunct? expr) (beval-or expr env))
     ((negation? expr) (beval-not expr env))
     ((implication? expr) (beval-imply expr env))
     ((equivalence? expr) (beval-equiv expr env))
     (else (error "beval:illegal syntax"))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Used in cond statement ;;;; 
-;;;; in beval 			  	;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Dispatch predicates not provided by
+;;;; underlying scheme implementation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (conjuct? expr) 
+(define (conjunct? expr) 
   (let ((function (car expr)))
     (eq? function 'and)))
 
-(define (disjuct? expr) 
+(define (disjunct? expr) 
   (let ((function (car expr)))
     (eq? function 'or)))
 
@@ -59,26 +65,40 @@
     (eq? function 'equiv)))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Evaluation Functions ;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Dispatch actions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;; obtain the binding of the variable in the env
+;;; returns 'error if the variable is unbound
 (define (lookup expr env)
   (let ((var-binding (assoc expr env)))
     (if (eq? var-binding #f)
-        'error
+        'error  
         (let ((binding-val (car (cdr var-binding))))
           binding-val))))
 
+;;; recursively evaluate a conjunction of boolean exprs
+;;; base-case is no further exprs to be evaluated (return #t)
+;;; recursive case is to:
+;;; 1. evaluate the first expr, using (beval ...),
+;;; 2. create a new conjunction and evaluate it with (beval-and ...)
+;;; 3. perform a logical and on the results of steps 1 and 2
 (define (beval-and expr env)
   (let ((boolean-expr (cdr expr)))
     (if (null? boolean-expr)
-        #t
+        #t   ; base case - return #t
         (let ((first-subexpr (car boolean-expr))
               (second-subexpr (cdr boolean-expr)))
           (and (beval first-subexpr env)
                (beval-and (cons 'and second-subexpr) env))))))
 
+;;; recursively evaluate a disjunction of boolean exprs
+;;; base-case is no further exprs to be evaluated (return #f)
+;;; recursive case is to:
+;;; 1. evaluate the first expr, using (beval ...),
+;;; 2. create a new disjunction and evaluate it with (beval-or ...)
+;;; 3. perform a logical or on the results of steps 1 and 2
 (define (beval-or expr env)
   (let ((boolean-expr (cdr expr)))
     (if (null? boolean-expr)
@@ -88,10 +108,16 @@
           (or (beval first-subexpr env)
               (beval-or (cons 'or second-subexpr) env))))))
 
+;;; invert the logical value of the expr evaluated using (beval ...)
 (define (beval-not expr env)
   (let ((boolean-expr (car (cdr expr))))
     (not (beval boolean-expr env))))
 
+;;; verify the logical implication of two sub-exprs of expr by computing 
+;;; the results of evaluating the two subexpressions using (beval ...) 
+;;; if the result of evaluating the first sub-expr is #t and the result
+;;; of evaluating the second sub-expr is #f, the implication is #f; 
+;;; otherwise it is #t
 (define (beval-imply expr env)
   (let* ((boolean-expr (cdr expr))
          (first-subexpr (car boolean-expr))
@@ -102,6 +128,11 @@
         #f
         #t)))
 
+;;; verify the logical equivalence of two sub-exprs of expr by computing 
+;;; the results of evaluating the two subexpressions using (beval ...)
+;;; if the result of evaluating both the first and second sub-exprs is #t
+;;; or the result of evaluating both the first and second sub-exprs is #f
+;;; then the equivalence is #t; otherwise it is #f
 (define (beval-equiv expr env)
   (let* ((boolean-expr (cdr expr))
          (first-subexpr (car boolean-expr))
@@ -115,35 +146,7 @@
             #f))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;   Helper Functions   ;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (true? x) 
-  (eq? x #t))
-
-(define (false? x) 
-  (not (eq? x #t)))
-
-(define (andmap f xs)
-  (cond ((null? xs) #t)
-        ((f (car xs))
-         (andmap f (cdr xs)))
-        (else #f)))
-
-(define (ormap f xs)
-  (cond ((null? xs) #f)
-        ((f (car xs)) #t)
-        (else (ormap f (cdr xs)))))
-
-(define (contains-only-true? list)
-  (andmap true? list))
-
-(define (contains-only-false? list)
-  (andmap false? list))
-
-  
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;   Sample Input       ;;;;
+;;;;   Sample Inputs      ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (run-test test-expression env)
@@ -168,8 +171,7 @@
   (run-test '(beval '(imply p q) '((p #f) (q #f))) my-env) ;Value: #t
   (run-test '(beval '(imply p q) '((p #f) (q #t))) my-env) ;Value: #t
   (run-test '(beval '(equiv (imply p q) (or (not p) q)) '((p #t) (q #f))) my-env) ;Value: #t
-  (run-test '(beval '(equiv (imply p q) (imply q p)) '((p #t) (q #f))) my-env) ;Value: t
-  (run-test '(contains-only-true? '(#t #f #t #t #t)) my-env))
+  (run-test '(beval '(equiv (imply p q) (imply q p)) '((p #t) (q #f))) my-env)) ;Value: t
 
   
   

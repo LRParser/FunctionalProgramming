2ea205a61f4d25c4c5273c11d521923bc406e560
Josh Datko <jbdatko@gmail.com>
2013-06-08 11:46:41 -0400

    Added text to answer the question.

--- SICP_4_68.scm
+++ SICP_4_68.scm
@@ -6,10 +6,7 @@
 
 
 (load "ch4-query.scm")
-
-
 (initialize-data-base microshaft-data-base)
-
 (query-driver-loop)
 
 ;;load the append-to-form (from the book)
@@ -18,16 +15,20 @@
 (assert! (rule (append-to-form (?u . ?v) ?y (?u . ?z))
 	       (append-to-form ?v ?y ?)))
 
-
+;; The reverse of the empty list is the empty list
 (assert! (rule (reverse () ())))
 
+;; The rest (cdr) of the list, when reversed and appended to the first (car) is
+;; the reversed list.
 (assert! (rule (reverse (?first . ?rest) ?reversed)
 	       (and (reverse ?rest ?rest-rev)
 		    (append-to-form ?rest-rev (?first) ?reversed))))
 
-
-;;TODO: Check if rules can answer both below:
-
 ;;Can your rules answer both (reverse (1 2 3) ?x) and (reverse ?x (1 2 3)) ?
 
-;;TODO: Do the same in Prolog.
+;;; Only (reverse (1 2 3) ?x) works since the other form (with ?x first)
+;;; results in an infinite loop.  Since the unbounded variable is in the left
+;;; most position and the search is depth first search, it will hit a stack
+;;; overflow from the constant recursion.
+
+;; See SICP_4_68.pl for the prolog version
